112f601e3026ec010e4f0d221d3c7c5ba3e6bac5
vamshivade <vamshivade2000@gmail.com>
2026-09-12 02:51:33 +0530

    Configure Firebase authentication and app UI

--- src/redux/appStore.js
+++ src/redux/appStore.js
@@ -1,11 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import useReducer from "./userSlice";
+import userReducer from "./userSlice";
 
 const appStore = configureStore({
   reducer: {
-    user: useReducer,
+    user: userReducer,
   },
-  devTools: process.env.NODE_ENV !== "production",
 });
 
 export default appStore;
